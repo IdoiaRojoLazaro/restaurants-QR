628aa7ff8d302b25150f80e3c5ad4ab6d7e396f1
IdoiaRojo <idoia@ifeelonline.com>
2026-02-17 09:59:41 +0000

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/restaurant-app-good/',
+  base: './',
   plugins: [react(), tailwindcss()],
 });
