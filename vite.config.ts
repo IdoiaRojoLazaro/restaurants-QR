b682c5f0b1366ee1158faeb60702d0823c389b68
IdoiaRojo <idoia@ifeelonline.com>
2026-02-17 10:03:11 +0000

    url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: './restaurants-QR',
   plugins: [react(), tailwindcss()],
 });
